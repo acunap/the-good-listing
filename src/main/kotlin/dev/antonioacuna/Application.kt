5ace80bfcb51483344f7db176bc93d44dd6043cf
acunap <hello@antonioacuna.dev>
2022-11-16 17:51:13 +0100

    Init Ktor project

---
+++ src/main/kotlin/dev/antonioacuna/Application.kt
@@ -0,0 +1,13 @@
+package dev.antonioacuna
+
+import io.ktor.server.application.*
+import dev.antonioacuna.plugins.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSecurity()
+    configureSerialization()
+    configureRouting()
+}
---
+++ src/test/kotlin/dev/antonioacuna/ApplicationTest.kt
@@ -0,0 +1,22 @@
+package dev.antonioacuna
+
+import io.ktor.http.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import dev.antonioacuna.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
